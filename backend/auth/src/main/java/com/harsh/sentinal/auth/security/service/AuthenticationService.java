2bb464a77c68d63a37e900d5767e799808909907
BERMUDA <bermuda@bermuda.bermuda>
2026-07-10 14:12:54 +0530

    BE/auth/ Added the jwt token authentication check and role based check.

--- backend/auth/src/main/java/com/harsh/sentinal/auth/security/service/AuthenticationService.java
+++ backend/auth/src/main/java/com/harsh/sentinal/auth/security/service/AuthenticationService.java
@@ -4,8 +4,11 @@ import com.harsh.sentinal.auth.security.jwt.JwtService;
 import com.harsh.sentinal.auth.security.principal.CustomUserDetails;
 import com.harsh.sentinal.auth.user.dto.LoginRequest;
 import com.harsh.sentinal.auth.user.dto.LoginResponse;
+import com.harsh.sentinal.auth.user.dto.UserInfoLoginResponse;
+import com.harsh.sentinal.auth.user.entity.User;
 import com.harsh.sentinal.auth.user.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +19,9 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
 
     private final AuthenticationManager authenticationManager;
     private final UserRepo userRepo;
@@ -32,8 +38,10 @@ public class AuthenticationService {
 
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
 
+        User user = userDetails.getUser();
+
         String authToken = jwtService.generateToken(userDetails.getUsername(), userDetails.getAuthorities().toString());
 
-        return new LoginResponse("Login Successfull", authToken);
+        return new LoginResponse(authToken, "Bearer", expiration, new UserInfoLoginResponse(user.getFirst_name(), user.getLast_name(), user.getEmail(), user.getRole()));
     }
 }
--- backend/auth/src/main/java/com/harsh/sentinal/auth/user/controller/UserController.java
+++ backend/auth/src/main/java/com/harsh/sentinal/auth/user/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.harsh.sentinal.auth.user.controller;
 
+import com.harsh.sentinal.auth.security.principal.CustomUserDetails;
 import com.harsh.sentinal.auth.security.service.AuthenticationService;
-import com.harsh.sentinal.auth.user.dto.LoginRequest;
-import com.harsh.sentinal.auth.user.dto.LoginResponse;
-import com.harsh.sentinal.auth.user.dto.RegisterRequest;
-import com.harsh.sentinal.auth.user.dto.RegisterResponse;
+import com.harsh.sentinal.auth.security.service.CustomUserDetailsService;
+import com.harsh.sentinal.auth.user.dto.*;
+import com.harsh.sentinal.auth.user.entity.User;
 import com.harsh.sentinal.auth.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,5 +31,23 @@ public class UserController {
     @PostMapping("/login")
     public LoginResponse loginUser(@Valid @RequestBody LoginRequest loginInfo){
         return authenticationService.loginUser(loginInfo);
+    }
+
+    @GetMapping("/me")
+    public UserInfoLoginResponse me(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        User user = customUserDetails.getUser();
+        return new UserInfoLoginResponse(user.getFirst_name(), user.getLast_name(), user.getEmail(), user.getRole());
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public String userEndpoint() {
+        return "Welcome User!";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminEndpoint() {
+        return "Welcome Admin!";
     }
 }
